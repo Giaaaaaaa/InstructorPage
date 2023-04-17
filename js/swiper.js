e3457bbb735eee283ea8109c7a5d405f079c7421
wenjia <dwgia027@gmail.com>
2023-04-17 20:23:16 +1000

    add testimonial autoplay feature

--- js/swiper.js
+++ js/swiper.js
@@ -1,3 +1,4 @@
+// 小滚动图
 var swiper = new Swiper(".mySwiper", {
     loop: true,
     spaceBetween: 10,
@@ -5,7 +6,15 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     freeMode: true,
     watchSlidesProgress: true,
+    // 增加自动播放功能
+    // 让当前的小滚动图保持原来的位置不动
+    centeredSlides: false,
+    // autoplay: {
+    //     delay: 2000,
+    //     disableOnInteraction: false,
+    //   },
 });
+// 大滚动图
 var swiper2 = new Swiper(".mySwiper2", {
     loop: true,
     spaceBetween: 10,
@@ -16,4 +25,10 @@ var swiper2 = new Swiper(".mySwiper2", {
     thumbs: {
         swiper: swiper,
     },
+    // 增加自动播放功能
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
 });
